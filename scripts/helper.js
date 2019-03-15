464b1552dc101a05b0ef6b6707ca115d62e8db3c
Riley Lu <luruiyi1988@hotmail.com>
2019-03-15 00:50:21 -0400

    update all

---
+++ scripts/helper.js
@@ -0,0 +1,6 @@
+class Helper {
+  playPauseAndUpdate (song) {
+    player.playPause(song);
+     $('#time-control .total-time').text(player.prettyTime(player.currentlyPlaying.duration));
+}}
+const helper = new Helper();
